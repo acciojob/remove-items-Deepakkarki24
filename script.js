f87c183911a7dd77de725ffbda3ca0cc396bb346
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 11:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let options = document.querySelector("#colorSelect");
+      let button = document.querySelector("input");
+
+      button.addEventListener("click", () => {
+        let index = options.selectedIndex;
+        options.remove(index);
+      });
